ab1071208415b1fa4bb108637ea6117a67fa274e
Rumaru001 <55047039+Rumaru001@users.noreply.github.com>
2019-09-23 18:41:39 +0300

    0021_Algotester

---
+++ 0021/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, count;
+    cin >> N;
+    count = N/500;
+    N = N - count*500;
+    int bills [] = {1, 2, 5, 10, 20, 50, 100, 200};
+    for (int i = 8; i >= 0 ; i--) {
+        while (N >= bills[i]){
+            ++count;
+            N = N - bills[i];
+        }
+
+    }
+
+    cout << count;
+
+    return 0;
+}
