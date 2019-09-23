09c4cec52bc0c070473a082f91df9e0e45e14c3c
Rumaru001 <55047039+Rumaru001@users.noreply.github.com>
2019-09-23 18:46:09 +0300

    Update main.cpp

--- 0021/main.cpp
+++ 0021/main.cpp
@@ -7,7 +7,7 @@ int main() {
     count = N/500;
     N = N - count*500;
     int bills [] = {1, 2, 5, 10, 20, 50, 100, 200};
-    for (int i = 8; i >= 0 ; i--) {
+    for (int i = 7; i >= 0 ; i--) {
         while (N >= bills[i]){
             ++count;
             N = N - bills[i];
